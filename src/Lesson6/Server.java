fedd6fdb6b7d2566dfd5f0e1e77da4a8cf9161c1
IvanAntasyuk <Pikin64@gmail.com>
2021-02-16 10:47:39 +0300

    dz6

--- src/Lesson6/Server.java
+++ src/Lesson6/Server.java
@@ -5,30 +5,50 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Server {
     public static void main(String[] args) {
-        Socket socket;
+        Socket socket = null;
+        Scanner scanner = new Scanner(System.in);
 
-      try(ServerSocket serverSocket = new ServerSocket(8082);){
-        System.out.println("Сервер начал свою работу.");
-        System.out.println("Ожидаю клиента.");
-        socket = serverSocket.accept();
-          System.out.println("Клиент подключился.");
-          DataInputStream dis = new DataInputStream(socket.getInputStream());
-          DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
-          while(true){
-              String msg = dis.readUTF();
-              if (msg.equalsIgnoreCase("/finish")){
-                  dos.writeUTF(msg);
-                  break;
-              }
-              dos.writeUTF(" Эхо " + msg);
-              dos.flush();
-          }
+        try (ServerSocket serverSocket = new ServerSocket(8082);) {
+            System.out.println("Сервер начал свою работу.");
+            System.out.println("Ожидаю клиента.");
+            socket = serverSocket.accept();
+            System.out.println("Клиент подключился." + socket.getRemoteSocketAddress());
+            DataInputStream dis = new DataInputStream(socket.getInputStream());
+            DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
+            Thread t1 = new Thread(() -> {
+                try {
+                    while (true) {
+                        dos.writeUTF(scanner.nextLine());
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+            t1.setDaemon(true);
+            t1.start();
 
-
-      }catch (IOException e){
-      }
+            while (true) {
+                String str = dis.readUTF();
+                if (str.equals("/close")) {
+                    System.out.println("Клиент покинул сервер");
+                    dos.writeUTF("/close");
+                    break;
+                } else {
+                    System.out.println("Клиент: " + str);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                socket.close();
+            } catch (IOException | NullPointerException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
